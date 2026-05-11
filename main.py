81a6687202fda64c8545aadf7f3829c04e8097f2
Alessa <203438900+Alessatabares@users.noreply.github.com>
2026-05-11 17:42:30 -0600

    Connect tools to LangChain agent

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
 
@@ -34,13 +35,14 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
+tools = [search_tool, wiki_tool, save_tool]
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
-    tools=[]
+    tools=tools
 )
 
-agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 raw_response = agent_executor.invoke({"query": "What is the capital of France?"})
 print(raw_response)
 
