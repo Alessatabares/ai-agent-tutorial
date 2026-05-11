f36d4d9d255ca84f96fef387b9a1d47253634f9c
Alessa <203438900+Alessatabares@users.noreply.github.com>
2026-05-11 17:07:13 -0600

    Add parser error handling

--- main.py
+++ main.py
@@ -43,3 +43,10 @@ agent = create_tool_calling_agent(
 agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
 raw_response = agent_executor.invoke({"query": "What is the capital of France?"})
 print(raw_response)
+
+try:
+    structured_response = parser.parse(raw_response.get("output"))
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
+
