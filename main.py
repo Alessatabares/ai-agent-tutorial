25f8731c0738fb0b54b5cb1aa4ee6addbed366f5
Alessa <203438900+Alessatabares@users.noreply.github.com>
2026-05-11 15:05:57 -0600

    Add prompt template with agent placeholders

--- main.py
+++ main.py
@@ -14,6 +14,21 @@ class ResearchResponse(BaseModel):
     tools_used: list[str]
     
 
-
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.7)
 parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
